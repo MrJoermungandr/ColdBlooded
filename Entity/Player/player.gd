1b8b9baa8e93daefaaf4d3b65caae79b5c6ce474
Jan Nothacker <git@jan-nothacker.de>
2024-08-30 21:34:39 +0200

    add coyote mechanics

--- Entity/Player/player.gd
+++ Entity/Player/player.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 @export
 var speed = 2000
 
+@export
+var coyote_time_seconds: float = 0.1
+var coyote_timer: Timer = Timer.new()
+var is_coyote_completed: bool = false
+
 @export_group("Jump")
 @export
 var jump_height_px: float = 90
@@ -32,12 +37,20 @@ var second_jump_gravity: float = ((-2.0 * second_jump_height_px) / (second_jump_
 @onready
 var second_fall_gravity: float = ((-2.0 * second_jump_height_px) / (second_jump_time_to_descend * second_jump_time_to_descend)) * -1
 
+var has_jumped: bool = false
 var has_double_jumped: bool = false
 
+func _ready() -> void:
+	coyote_timer.one_shot = true
+	coyote_timer.wait_time = coyote_time_seconds
+	coyote_timer.timeout.connect(_on_coyote_complete)
+	add_child(coyote_timer)
+
 func _physics_process(delta: float) -> void:
-	var is_on_ground = is_on_floor()
+	var is_on_ground = calc_is_on_ground()
 	if is_on_ground:
 		has_double_jumped = false
+		has_jumped = false
 	_handle_input(delta, is_on_ground)
 	move_and_slide()
 
@@ -49,6 +62,7 @@ func _handle_input(delta: float, is_on_ground: bool):
 	if Input.is_action_just_pressed("move_jump"):
 		if is_on_ground:
 			velocity.y = jump_velocity
+			has_jumped = true
 		elif not has_double_jumped:
 			velocity.y = second_jump_velocity
 			has_double_jumped = true
@@ -58,3 +72,18 @@ func get_current_gravity() -> float:
 		return second_jump_gravity if has_double_jumped else jump_gravity
 	else:
 		return second_fall_gravity if has_double_jumped else fall_gravity
+
+func calc_is_on_ground() -> bool:
+	if is_on_floor():
+		velocity.y = 0	#might be better to put this somewhere else
+		is_coyote_completed = false
+		return true
+	elif is_coyote_completed or has_jumped or has_double_jumped:	#prevents gravity calculation being inaccurate
+		return false
+	elif coyote_timer.is_stopped():
+		coyote_timer.start()
+		return true
+	return true
+
+func _on_coyote_complete():
+	is_coyote_completed = true
