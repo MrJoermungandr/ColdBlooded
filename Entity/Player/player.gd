f1124d6abd32bdf3e30edadfae11ebfcc404ae09
Jan Nothacker <git@jan-nothacker.de>
2024-08-30 19:29:53 +0200

    add very simpleton movement and test level

--- Entity/Player/player.gd
+++ Entity/Player/player.gd
@@ -1,8 +1,25 @@
 extends CharacterBody2D
 
 @export
-var speed = 20
+var speed = 2000
+
+var has_double_jumped: bool = false
 
 func _process(delta: float) -> void:
-	var desired_dir = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
-	velocity = desired_dir * speed * delta
+	if is_on_floor():
+		has_double_jumped = false
+	_handle_input(delta)
+	move_and_slide()
+	if Input.is_action_just_pressed("move_jump"):
+		if is_on_floor():
+			velocity.y -= 50
+		elif not has_double_jumped:
+			velocity.y -= 50
+			has_double_jumped = true
+
+
+func _handle_input(delta: float):
+	var hor_input = Input.get_axis("move_left", "move_right")
+	velocity.x = hor_input * speed
+	if not is_on_floor():
+		velocity.y += delta * 10
