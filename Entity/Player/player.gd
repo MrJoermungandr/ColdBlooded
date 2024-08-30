3fe34c665ac815ef0c591dd0e8c306e9f53be098
Jan Nothacker <git@jan-nothacker.de>
2024-08-30 20:44:30 +0200

    double jump with it's own physics

--- Entity/Player/player.gd
+++ Entity/Player/player.gd
@@ -5,18 +5,32 @@ var speed = 2000
 
 @export_group("Jump")
 @export
-var jump_height: float = 100
+var jump_height_px: float = 90
 @export
 var jump_time_to_peak: float = 0.5
 @export
-var jump_time_to_descend: float = 0.3
+var jump_time_to_descend: float = 0.4
+
+@export
+var second_jump_height_px: float = 54
+@export
+var second_jump_time_to_peak: float = 0.4
+@export
+var second_jump_time_to_descend: float = 0.3
 
 @onready
-var jump_velocity: float = ((2.0 * jump_height) / jump_time_to_peak) * -1
+var jump_velocity: float = ((2.0 * jump_height_px) / jump_time_to_peak) * -1
 @onready
-var jump_gravity: float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1
+var jump_gravity: float = ((-2.0 * jump_height_px) / (jump_time_to_peak * jump_time_to_peak)) * -1
 @onready
-var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_descend * jump_time_to_descend)) * -1
+var fall_gravity: float = ((-2.0 * jump_height_px) / (jump_time_to_descend * jump_time_to_descend)) * -1
+
+@onready
+var second_jump_velocity: float = ((2.0 * second_jump_height_px) / second_jump_time_to_peak) * -1
+@onready
+var second_jump_gravity: float = ((-2.0 * second_jump_height_px) / (second_jump_time_to_peak * second_jump_time_to_peak)) * -1
+@onready
+var second_fall_gravity: float = ((-2.0 * second_jump_height_px) / (second_jump_time_to_descend * second_jump_time_to_descend)) * -1
 
 var has_double_jumped: bool = false
 
@@ -36,8 +50,11 @@ func _handle_input(delta: float, is_on_floor: bool):
 		if is_on_floor:
 			velocity.y = jump_velocity
 		elif not has_double_jumped:
-			velocity.y = jump_velocity
+			velocity.y = second_jump_velocity
 			has_double_jumped = true
 
 func get_current_gravity() -> float:
-	return jump_gravity if velocity.y < 0.0 else fall_gravity # ternary statement is back, hell yeah!
+	if velocity.y < 0.0:	# ternary statement is back, hell yeah!
+		return second_jump_gravity if has_double_jumped else jump_gravity
+	else:
+		return second_fall_gravity if has_double_jumped else fall_gravity
