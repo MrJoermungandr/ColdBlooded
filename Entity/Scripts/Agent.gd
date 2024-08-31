918e372987101af9ad650f1be2d2f5743437c412
JaN0h4ck <git@jan-nothacker.de>
2024-08-31 23:12:02 +0200

    create agent knockback

--- Entity/Scripts/Agent.gd
+++ Entity/Scripts/Agent.gd
@@ -7,6 +7,7 @@ var entity_resource: EntityResource
 signal death
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var bt_player: BTPlayer = $BTPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,7 +18,16 @@ func _process(delta: float) -> void:
 	pass
 
 func _take_damage(amount: int, knockback: Vector2):
+	apply_knockback(knockback)
 	if entity_resource.health - amount <= 0:
 		death.emit()
 		queue_free() #TODO proper dying
 	entity_resource.health -= amount
+
+func apply_knockback(knockback: Vector2, frames: int = 10):
+	if knockback.is_zero_approx():
+		return
+	for i in range(frames):
+		velocity = lerp(velocity, knockback, 0.2)
+		move_and_slide()
+		await get_tree().physics_frame
