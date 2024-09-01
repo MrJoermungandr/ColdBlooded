f00daeaa825e611f6a2287482eb14b4c20ba2090
JaN0h4ck <git@jan-nothacker.de>
2024-09-01 12:40:12 +0200

    test damage interaction

--- Entity/Player/player.gd
+++ Entity/Player/player.gd
@@ -75,7 +75,7 @@ func _handle_input(delta: float, is_on_ground: bool):
 	if not is_on_ground:
 		velocity.y += get_current_gravity() * delta
 	_handle_jump_and_glide(is_on_ground)
-	if Input.is_action_pressed(&"attack_primary"):
+	if Input.is_action_just_pressed(&"attack_primary"):
 		animation_player.play(&"attack")
 		await animation_player.animation_finished
 
---
+++ Level/EnemyTest/dummy_enemy.gd
@@ -0,0 +1,16 @@
+extends MeshInstance2D
+
+@export
+var entity_resource: EntityResource
+
+@onready 
+var label: Label = $Label
+
+func _process(_delta: float) -> void:
+	label.text = str(entity_resource.health)
+
+func take_damage(amount: int, _knockback: Vector2):
+	if entity_resource.health - amount <= 0:
+		queue_free()
+	else:
+		entity_resource.health -= amount
