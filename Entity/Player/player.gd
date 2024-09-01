884c88a58fdf44417a674987954c94d985a5476b
JaN0h4ck <git@jan-nothacker.de>
2024-09-01 21:29:27 +0200

    code cleanup

--- Entity/Player/player.gd
+++ Entity/Player/player.gd
@@ -68,19 +68,7 @@ func _ready() -> void:
 	vertical_pinch_collision.set_deferred("disabled", true)
 	_init_state_machine()
 
-#region states
-
-func _init_state_machine():
-	#state_machine = LimboHSM.new()
-	#add_child(state_machine)
-	
-	#var idle_state = LimboState.new().named("idle").call_on_enter(_idle_ready).call_on_update(_idle_update)
-	#var move_state = LimboState.new().named("move").call_on_enter(_move_ready).call_on_update(_move_update)
-	#var attack_state = LimboState.new().named("attack").call_on_enter(_attack_ready).call_on_update(_attack_update)
-	#state_machine.add_child(idle_state)
-	#state_machine.add_child(move_state)
-	#state_machine.add_child(attack_state)
-	
+func _init_state_machine():	
 	state_machine.add_transition(idle_state, move_state, &"move_started")
 	state_machine.add_transition(move_state, idle_state, &"move_ended")
 	state_machine.add_transition(state_machine.ANYSTATE, attack_state, &"atk_started")
@@ -89,46 +77,6 @@ func _init_state_machine():
 	
 	state_machine.initialize(self)
 	state_machine.set_active(true)
-
-#func _idle_ready():
-	#label.text = "idle"
-
-#func _idle_update(delta: float):
-	#if not calc_is_on_ground():
-		#velocity.y += get_current_gravity() * delta
-		#move_and_slide()
-		#return
-	#var input: Vector2 = Input.get_vector(&"move_left", &"move_right", &"move_back", &"move_jump")
-	#if not input.is_zero_approx():
-		#_handle_input(delta, calc_is_on_ground())
-		#move_and_slide()
-		#state_machine.dispatch(&"move_started")
-		#return
-	#if Input.is_action_just_pressed("attack_primary"):
-		#state_machine.dispatch(&"atk_started")
-
-#func _move_ready():
-	#label.text = "move"
-
-#func _move_update(delta: float):
-	#var is_on_ground = calc_is_on_ground()
-	#if is_on_ground:
-		#has_double_jumped = false
-		#has_jumped = false
-	#_handle_input(delta, is_on_ground)
-	#move_and_slide()
-	#if velocity.is_zero_approx() and is_on_ground:
-		#state_machine.dispatch(&"move_ended")
-
-#func _attack_ready():
-	#label.text = "attack"
-	#animation_player.play(&"attack", -1, 2.0)
-	#await animation_player.animation_finished
-	#state_machine.dispatch(state_machine.EVENT_FINISHED)
-#
-#func _attack_update(delta: float):
-	#pass
-#endregion
 
 func _handle_input(delta: float, is_on_ground: bool):
 	var hor_input = Input.get_axis("move_left", "move_right")
