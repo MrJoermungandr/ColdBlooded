cd9e7d18eacfbc841f59e8d57ff9950112e63cf4
JaN0h4ck <git@jan-nothacker.de>
2024-09-01 11:34:13 +0200

    give hitbox it's properties

--- Entity/Scripts/hitbox.gd
+++ Entity/Scripts/hitbox.gd
@@ -2,11 +2,16 @@ class_name Hitbox
 extends Area2D
 #deals damage
 
+@export
+var entity_resource: EntityResource #for damage var
+
+@export_group("Collision")
+@export_flags_2d_physics
+var collision_layer_override: int = 2
+@export_flags_2d_physics
+var collision_mask_override: int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	collision_layer = collision_layer_override
+	collision_mask = collision_mask_override
