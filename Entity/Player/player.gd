459f1ac9ab14c35b9e2b392750629c3049158616
JaN0h4ck <git@jan-nothacker.de>
2024-09-02 17:43:42 +0200

    change player behavoiur in air

--- Entity/Player/player.gd
+++ Entity/Player/player.gd
@@ -79,10 +79,11 @@ func _init_state_machine():
 
 func _handle_input(delta: float, is_on_ground: bool):
 	var hor_input = Input.get_axis("move_left", "move_right")
-	velocity.x = hor_input * speed
 	if not is_on_ground:
 		velocity.y += get_current_gravity() * delta
+		velocity.x = velocity.slerp(Vector2(velocity.x if is_zero_approx(hor_input) else hor_input * speed, velocity.y) * 0.9, .2).x
 	else:
+		velocity.x = hor_input * speed
 		velocity.y = 0
 	_handle_jump_and_glide(is_on_ground)
 
