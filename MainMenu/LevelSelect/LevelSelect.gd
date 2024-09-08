bd67144e7f3877fecb74972b826de82c4f99b73e
Kilian Strunz <voltox@gmx.de>
2024-09-08 19:31:10 +0200

    initial backgrounds working

--- MainMenu/LevelSelect/LevelSelect.gd
+++ MainMenu/LevelSelect/LevelSelect.gd
@@ -3,15 +3,15 @@ extends PanelContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_node("VerticalEntrys/LevelName/Label").text=get_name()
-	get_node("VerticalEntrys/PlayLevel/Button").pressed.connect(on_play_pressed)
+	get_node("MarginContainer/VerticalEntrys/LevelName/Label").text=get_parent().get_name()
+	get_node("MarginContainer/VerticalEntrys/PlayLevel/Button").pressed.connect(on_play_pressed)
 	if GameManger.save.runs.has(get_name()):
-		get_node("VerticalEntrys/LocalRecord").record_time=GameManger.save.runs[get_name()].level_time
+		get_node("MarginContainer/VerticalEntrys/LocalRecord").record_time=GameManger.save.runs[get_name()].level_time
 		
 	var request=HTTPRequest.new()
 	add_child(request)
 	request.request_completed.connect(on_leaderboard_entries_retrieved)
-	request.request(LeaderboardManager.API_URL+"/times/"+get_name())
+	request.request(LeaderboardManager.API_URL+"/times/"+get_parent().get_name())
 	await request.request_completed
 	remove_child(request)
 
@@ -20,7 +20,7 @@ func on_leaderboard_entries_retrieved(result,response_code,headers,body):
 		var parsed_body=JSON.parse_string(body.get_string_from_utf8())
 		var iter_count=1
 		for entry in parsed_body:
-			var node=get_node("VerticalEntrys/TopPositions/LeaderboardEntry"+str(iter_count))
+			var node=get_node("MarginContainer/VerticalEntrys/TopPositions/LeaderboardEntry"+str(iter_count))
 			node.record_position=iter_count
 			node.record_name=entry.username
 			node.record_time=entry.time
@@ -29,5 +29,5 @@ func on_leaderboard_entries_retrieved(result,response_code,headers,body):
 				break
 
 func on_play_pressed():
-	var scene=load("res://Level/"+get_name()+".tscn")
+	var scene=load("res://Level/"+get_parent().get_name()+".tscn")
 	get_tree().change_scene_to_packed(scene)
