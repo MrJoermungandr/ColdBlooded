7c81e855b2292f1b90417b57c0c57f53c7ed1402
JaN0h4ck <git@jan-nothacker.de>
2024-09-11 17:45:44 +0200

    parameterize da camera

--- Scripts/lookahead_camera_2d.gd
+++ Scripts/lookahead_camera_2d.gd
@@ -5,6 +5,10 @@ var player: CharacterBody2D = $".."
 
 @export
 var lerp_speed: float = .8
+@export
+var horizontal_lookahead_px: float = 90
+@export
+var vertical_lookahead_px: float = 72
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,12 +18,12 @@ func _physics_process(delta: float) -> void:
 	if is_zero_approx(player.velocity.x):
 		offset.x = lerp(offset.x, 0.0, delta)
 	elif player.velocity.x > 0:
-		offset.x = lerp(offset.x, 90.0, delta * lerp_speed)
+		offset.x = lerp(offset.x, horizontal_lookahead_px, delta * lerp_speed)
 	else:
-		offset.x = lerp(offset.x, -90.0, delta * lerp_speed)
+		offset.x = lerp(offset.x, -horizontal_lookahead_px, delta * lerp_speed)
 	if is_zero_approx(player.velocity.y):
 		offset.y = lerp(offset.y, 0.0, delta)
 	elif player.velocity.y < 0: # UP
-		offset.y = lerp(offset.y, -72.0, delta * lerp_speed)
+		offset.y = lerp(offset.y, -vertical_lookahead_px, delta * lerp_speed)
 	else:
-		offset.y = lerp(offset.y, 72.0, delta * lerp_speed)
+		offset.y = lerp(offset.y, vertical_lookahead_px, delta * lerp_speed)
