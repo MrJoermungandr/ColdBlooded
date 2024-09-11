3aa2bb299b9291f9546fa86fc48ceb34cc1db7b0
JaN0h4ck <git@jan-nothacker.de>
2024-09-11 23:50:48 +0200

    add KeyPersistence Script

--- Ui/SettingsMenu/settings_menu.gd
+++ Ui/SettingsMenu/settings_menu.gd
@@ -2,20 +2,15 @@ extends MarginContainer
 
 signal exit
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VBoxContainer/TabContainer/Graphics/MarginContainer/VBoxContainer/FullscreenToggle.button_pressed = SettingsPersistence.use_fullscreen
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_cancel"):
 		exit.emit()
 
-
 func _on_back_button_pressed() -> void:
 	exit.emit()
-
 
 func _on_fullscreen_toggle_toggled(toggled_on: bool) -> void:
 	SettingsPersistence.use_fullscreen = toggled_on
