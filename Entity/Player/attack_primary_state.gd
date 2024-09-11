62738f781218096d42af8e423eeed3ae79ddfa6a
JaN0h4ck <git@jan-nothacker.de>
2024-09-11 22:05:30 +0200

    moving during animation

--- Entity/Player/attack_primary_state.gd
+++ Entity/Player/attack_primary_state.gd
@@ -12,3 +12,16 @@ func _enter() -> void:
 	agent.animation_player.play(&"attack", -1, 2.0)
 	await agent.animation_player.animation_finished
 	agent.state_machine.dispatch(EVENT_FINISHED)
+
+func _update(delta: float) -> void:
+	var is_on_ground = agent.calc_is_on_ground()
+	if is_on_ground:
+		agent.has_double_jumped = false
+		agent.has_jumped = false
+	agent._handle_input(delta, is_on_ground)
+	if Input.is_action_just_pressed(&"attack_primary"):
+		dispatch(&"atk_started")
+		return
+	agent.move_and_slide()
+	if agent.velocity.is_zero_approx() and is_on_ground:
+		dispatch(&"move_ended")
