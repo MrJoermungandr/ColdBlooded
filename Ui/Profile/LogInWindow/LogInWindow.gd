f485f695813292d7d13e67d9e0db8bb51733d3fa
Kilian Strunz <voltox@gmx.de>
2024-09-12 13:42:28 +0200

    handle signup login

--- Ui/Profile/LogInWindow/LogInWindow.gd
+++ Ui/Profile/LogInWindow/LogInWindow.gd
@@ -4,8 +4,13 @@ extends CenterContainer
 @onready
 var OkButton=get_node("Panel/MarginContainer/VBoxContainer/HBoxContainer/Button")
 
+@onready
+var CloseButton=get_node("Panel/MarginContainer/VBoxContainer/HBoxContainer2/Button")
+
 var username:String=""
 var password:String=""
+
+var signup:bool=false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,6 +19,10 @@ func _ready() -> void:
 	get_node("Panel/MarginContainer/VBoxContainer/Password/LineEdit").text_changed.connect(on_password_changed)
 	
 	LeaderboardManager.login_status_changed.connect(on_login_status_changed)
+	
+	CloseButton.pressed.connect(on_closed)
+	get_node("Panel/MarginContainer/VBoxContainer/HBoxContainer2/HBoxContainer/CheckButton").toggled.connect(on_toggled)
+	get_node("Panel/MarginContainer/VBoxContainer/Repeat").visible=false
 	
 func on_username_changed(new:String):
 	#TODO validation
@@ -24,9 +33,21 @@ func on_password_changed(new:String):
 	
 func on_pressed_ok():
 	#TODO validation
-	LeaderboardManager.login(username,password)
+	if signup:
+		LeaderboardManager.signup(username,password)
+	else:
+		LeaderboardManager.login(username,password)
 
 
 func on_login_status_changed(new:bool):
 	if new:
 		visible=false
+
+
+func on_closed():
+	visible=false
+
+
+func on_toggled(new:bool):
+	signup=new
+	get_node("Panel/MarginContainer/VBoxContainer/Repeat").visible=signup
