b29d33818e5ffcd167df4802f973c7a4dd891a2e
defnegoncu <goncu.defne@gmail.com>
2024-09-13 12:31:09 +0300

    settings saved

--- Ui/SoundSettings.gd
+++ Ui/SoundSettings.gd
@@ -1,22 +1,25 @@
 extends TabBar
 
+@onready var music_slider: HSlider = $MarginContainer/VBoxContainer/MusicSlider
+@onready var sound_slider: HSlider = $MarginContainer/VBoxContainer/SoundSlider
+@onready var annoyance_slider: HSlider = $MarginContainer/VBoxContainer/AnnoyanceSlider
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	music_slider.set_value_no_signal(SettingsPersistence.music_volume)
+	sound_slider.set_value_no_signal(SettingsPersistence.sound_volume)
+	annoyance_slider.set_value_no_signal(SettingsPersistence.annoyance_volume)
 
 func _on_music_slider_changed(value :float):
 	AudioManager.set_music_volume(value)
+	SettingsPersistence.music_volume=value
 func _on_sound_slider_changed(value:float):
 	AudioManager.set_sound_volume(value)
+	SettingsPersistence.sound_volume=value
 func _on_annoyance_slider_changed(value:float):
 	AudioManager.set_annoyance_volume(value)
-	
+	SettingsPersistence.annoyance_volume=value
 func _on_sound_slider_stop(changed:bool):
 	AudioManager.sound_player_death()
 func _on_annoyance_slider_stop(changed:bool):
