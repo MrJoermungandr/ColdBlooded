ea1f938515e807703066dc4c4bd44c21af25106b
Kilian Strunz <voltox@gmx.de>
2024-09-13 15:12:27 +0200

    fix weird cp behaviour in level 1

--- Level/Asset/Checkpoint/Checkpoint.gd
+++ Level/Asset/Checkpoint/Checkpoint.gd
@@ -5,12 +5,16 @@ extends Node2D
 @onready
 var popup=preload("res://Level/Asset/Checkpoint/ActivatedPopup.tscn")
 
+var level
+
 func _ready() -> void:
 	get_node("Area2D").body_entered.connect(on_checkpoint_collected)
+	var count=get_tree().root.get_child_count()
+	level=get_tree().root.get_child(count-1)
 
 
 func on_checkpoint_collected(_ja):
-	var count=get_tree().root.get_child_count()
-	var level=get_tree().root.get_child(count-1)
-	level.checkpoint_pickup(global_position)
+	if !is_node_ready(): return
+	level.checkpoint_pickup(self.global_position)
+	#print(self.global_position)
 	add_child(popup.instantiate())
